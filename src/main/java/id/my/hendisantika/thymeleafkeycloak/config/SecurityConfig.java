ffa05347bb4b812ffe6626ef681487a9dddc00d6
hendisantika <hendisantika@yahoo.co.id>
2024-11-25 06:35:24 +0700

    Add realmRolesAuthoritiesConverter in SecurityConfig🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/thymeleafkeycloak/config/SecurityConfig.java
+++ src/main/java/id/my/hendisantika/thymeleafkeycloak/config/SecurityConfig.java
@@ -1,13 +1,18 @@
 package id.my.hendisantika.thymeleafkeycloak.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
 import java.util.Collection;
+import java.util.List;
 import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Stream;
 
 /**
  * Created by IntelliJ IDEA.
@@ -25,6 +30,17 @@ import java.util.Map;
 public class SecurityConfig {
 
     interface AuthoritiesConverter extends Converter<Map<String, Object>, Collection<GrantedAuthority>> {
+    }
+
+    @Bean
+    AuthoritiesConverter realmRolesAuthoritiesConverter() {
+        return claims -> {
+            final var realmAccess = Optional.ofNullable((Map<String, Object>) claims.get("realm_access"));
+            final var roles =
+                    realmAccess.flatMap(map -> Optional.ofNullable((List<String>) map.get("roles")));
+            return roles.map(List::stream).orElse(Stream.empty()).map(SimpleGrantedAuthority::new)
+                    .map(GrantedAuthority.class::cast).toList();
+        };
     }
 
 }
